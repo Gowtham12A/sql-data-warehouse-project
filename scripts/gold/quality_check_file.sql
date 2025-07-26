3374af818cae636035ad5acc6922e53600ed667e
Gowtham12A <gowthamsrk99@gmail.com>
2025-07-26 17:02:24 +0530

    Create quality_check_file.sql

---
+++ scripts/gold/quality_check_file.sql
@@ -0,0 +1,25 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency,
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+-- Foreign key integrity (Dimensions)
+SELECT *
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL
